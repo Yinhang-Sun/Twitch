8fcb115613c6bbebff3b20cce8fb7a54fe7bb593
Yinhang-Sun <98371617+Yinhang-Sun@users.noreply.github.com>
2022-08-29 17:48:56 -0700

    Update FavoriteRequestBody.java

--- twitchbe/src/main/java/com/laioffer/jupiter/entity/request/FavoriteRequestBody.java
+++ twitchbe/src/main/java/com/laioffer/jupiter/entity/request/FavoriteRequestBody.java
@@ -12,20 +12,3 @@ public class FavoriteRequestBody {
         return favoriteItem;
     }
 }
-
-
-//package com.laioffer.jupiter.entity.request;
-//
-//import com.laioffer.jupiter.entity.db.Item;
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//
-//public class FavoriteRequestBody {
-//
-//    @JsonProperty("favorite")
-//    private Item favoriteItem;
-//
-//    public Item getFavoriteItem() {
-//        return favoriteItem;
-//    }
-//}
-//
